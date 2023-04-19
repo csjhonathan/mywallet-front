2274f6733ecb5a3e4e0d734798608e0f46f21479
csjhonathan <jhonathancarv.s@gmail.com>
2023-04-19 14:35:34 -0300

    signup is ready

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,16 +1,65 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import MyWalletLogo from '../components/MyWalletLogo';
-
+import axios from 'axios';
+import { useState } from 'react';
 export default function SignUpPage() {
+    const [form, setForm] = useState({nome : '', email : '', senha : '', confirma : ''});
+    
+    function handleForm(key, value){
+        setForm({...form, [key] : value});
+    }
+    async function registerUser(e){
+        e.preventDefault();
+        if(form.confirma !==  form.senha) {
+            return alert('As senhas devem ser iguais!');
+        }
+        const body = {
+            nome : form.nome,
+            email : form.email,
+            senha : form.senha
+        };
+        try{
+            await axios.post(`${process.env.REACT_APP_API_URL}/sign-up`, body);
+
+        }catch(err){
+            alert(err.response.data.message);
+        }
+    }
     return (
         <SingUpContainer>
-            <form>
+            <form onSubmit={(e) => registerUser(e)}>
                 <MyWalletLogo />
-                <input placeholder="Nome" type="text" />
-                <input placeholder="E-mail" type="email" />
-                <input placeholder="Senha" type="password" autoComplete="new-password" />
-                <input placeholder="Confirme a senha" type="password" autoComplete="new-password" />
+                <input placeholder="Nome" 
+                    type="text" name='nome' 
+                    value={form.nome} 
+                    onChange={(e) => handleForm(e.target.name, e.target.value)}
+                    required
+                />
+                <input placeholder="E-mail" 
+                    type="email" name='email' 
+                    value = {form.email} 
+                    onChange={(e) => handleForm(e.target.name, e.target.value)}
+                    required
+                />
+                <input placeholder="Senha" 
+                    type="password" 
+                    name='senha' 
+                    autoComplete="new-password" 
+                    value = {form.senha} 
+                    onChange={(e) => handleForm(e.target.name, e.target.value)}
+                    required
+                    minLength={3}
+                />
+                <input placeholder="Confirme a senha" 
+                    name='confirma' 
+                    type="password" 
+                    autoComplete="new-password" 
+                    value={form.confirma} 
+                    onChange={(e) => handleForm(e.target.name, e.target.value)}
+                    required
+                    minLength={3}
+                />
                 <button>Cadastrar</button>
             </form>
 
--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     * {
@@ -53,6 +53,6 @@ const GlobalStyle = createGlobalStyle`
         text-decoration: none;
         padding-top: 30px;
     }
-`
+`;
 
-export default GlobalStyle
+export default GlobalStyle;
